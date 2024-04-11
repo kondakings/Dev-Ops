921ae58806dd3d82fafaf5adee96ba58075dcfdd
Ubuntu <ubuntu@ip-172-31-22-105.eu-west-2.compute.internal>
2024-04-11 20:27:02 +0000

    adding shell files

---
+++ reddy-shell.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "my name is kondigadu"
+echo "nadi niku enduku ra "
+a=10
+b=5
+
+if [$a>$b]  (if else loop)
+then 
+	echo " a is greater than b"
+else 
+	echo "b is greater than a"
+fi
+
+for i in {1.100}; do echo $1; done
+
+for 
+do 
+	increment
+
