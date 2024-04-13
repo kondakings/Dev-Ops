436f35d7af469cd84f2cad9a81a2499da80ab1ca
Ubuntu <ubuntu@ip-172-31-22-105.eu-west-2.compute.internal>
2024-04-13 20:09:59 +0000

    adding one more file

---
+++ reddy-shell2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "idhi rendo konda gadu"
+marks=90
+if[$marks>85]
+then 
+	echo "Grade A"
+	elfi[$marks>75]
+then
+	echo"Grade B"
+	elfi[$marks>65]
+then
+	echo"Grade C"
+else 
+	echo "Fail"
+fi
+
